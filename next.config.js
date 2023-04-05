5ce91fe28baf9e30d8e4dfab4d7514f0701b021e
Anuj Pramod Singh <anujsingh49244@gmail.com>
2023-04-05 14:42:15 +0530

    Setup linting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages']  // Only run ESLint on the 'pages' directories during production builds (next build)
+  }
 }
 
 module.exports = nextConfig
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{
+        background: 'white',
+        color: 'black',
+      }}>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
