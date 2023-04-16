b46a5090c20fd1e5a59977ef49a89ffb0603e694
comaecod <vishnuthecoder@gmail.com>
2023-04-16 16:48:46 +0530

    Add Prisma + MongoDB

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1 className='text-3xl font-bold flex-row'>Hello world!</h1>;
+import { server } from "@/config";
+
+export default function Home({
+  data,
+}: {
+  data: {
+    name: string;
+  };
+}) {
+  return <h1 className='text-3xl font-bold flex-row'>{data.name}</h1>;
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${server}/api/hello`);
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
