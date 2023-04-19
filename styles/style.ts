1086f74c06d985779b1632e851bcf4765ad30d81
Anuj Pramod Singh <98264250+anujSingh-A-writer@users.noreply.github.com>
2023-04-19 11:44:07 +0530

    Style: auth page

---
+++ styles/style.ts
@@ -0,0 +1,16 @@
+export const layout = {
+  flex: {
+    row: {
+      center: "flex flex-row items-center justify-center ",
+      spaceEvenly: "flex flex-row items-center justify-evenly ",
+      left: "flex flex-row items-start justify-start ",
+      right: "flex flex-row items-end justify-end ",
+    },
+
+    col: {
+      center: "flex flex-col items-center justify-center ",
+      left: "flex flex-col items-start justify-start ",
+      right: "flex flex-col items-end justify-end ",
+    },
+  },
+};
