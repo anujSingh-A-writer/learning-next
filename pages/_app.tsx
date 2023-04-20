0745ee8c65fac97e9e3c12a6ba1dc97ea5fda5e0
Anuj Pramod Singh <98264250+anujSingh-A-writer@users.noreply.github.com>
2023-04-20 15:37:11 +0530

    Style: dashboard frame

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Layout from "@/components/layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  return (
+    <Layout path={router.pathname}>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
--- styles/style.ts
+++ styles/style.ts
@@ -3,6 +3,7 @@ export const layout = {
     row: {
       center: "flex flex-row items-center justify-center ",
       spaceEvenly: "flex flex-row items-center justify-evenly ",
+      spaceBetween: "flex flex-row justify-between ",
       left: "flex flex-row items-start justify-start ",
       right: "flex flex-row items-end justify-end ",
     },
@@ -10,6 +11,7 @@ export const layout = {
     col: {
       center: "flex flex-col items-center justify-center ",
       left: "flex flex-col items-start justify-start ",
+      spaceBetween: "flex flex-col justify-between ",
       right: "flex flex-col items-end justify-end ",
     },
   },
